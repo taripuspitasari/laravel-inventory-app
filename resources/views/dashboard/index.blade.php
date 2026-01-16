0104cdf8f42bdf8926a6267ad90762a494d32f14
Tari Puspita Sari <taripuspitasari12@gmail.com>
2026-01-16 14:41:06 +0700

    feat: update dashboard home chart

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -25,36 +25,16 @@
 </div>
 
 
-<section class="flex flex-col gap-4 justify-evenly md:flex-row mt-4">
-  <div class="flex flex-col justify-center items-center">
-    <h3 class="text-2xl font-bold  -mt-2 mb-2">Inventory Values</h3>
-    <div class="w-80">
-    <canvas id="stockChart"></canvas>
-    </div>
+<section class="flex flex-col gap-4 justify-evenly items-start md:flex-row mt-4">
+  <div class="flex-1">
+    <h3 class="text-2xl font-bold -mt-2 text-center">Top 8 Products by Sales</h3>
+    <canvas id="topProductChart"></canvas>
   </div>
 
-  <div>
-    <h3 class="text-2xl font-bold -mt-2 mb-2 text-center">Top 8 Products by Sales</h3>
-    <div class="overflow-x-auto rounded-md border border-base-content/5 bg-base-100 shadow">
-      <table class="table table-sm w-full">
-        <!-- head -->
-        <thead>
-          <tr>
-            <th>Rank</th>
-            <th>Product Name</th>
-            <th>Units Sold</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($topProducts as $index => $product)
-          <tr>
-            <th>{{ $index + 1 }}</th>
-            <td>{{ $product->name }}</td>
-            <td>{{ $product->total_qty_sold }}</td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
+  <div class="flex flex-col justify-center items-center">
+    <h3 class="text-2xl font-bold -mt-2">Inventory Values</h3>
+    <div class="w-80">
+    <canvas id="stockChart"></canvas>
     </div>
   </div>
 </section>
@@ -71,6 +51,25 @@
         data: @json([$totalSold, $totalStock])
       }]
     },
+  });
+
+  const cty = document.getElementById('topProductChart');
+
+  new Chart(cty, {
+    type: 'bar',
+    data: {
+      labels: @json($topProducts->pluck('name')),
+      datasets: [{
+        backgroundColor: ["#93c5fd", "#1d4ed8"],
+        data: @json($topProducts->pluck('total_qty_sold'))
+      }]
+    },
+    options: {
+    indexAxis: 'y',
+    plugins: {
+      legend: { display: false },
+    },
+  }
   });
 </script>
 @endsection
